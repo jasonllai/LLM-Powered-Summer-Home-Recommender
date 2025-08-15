bca8b8b12480ed3e837a4568e73381b65c7c3993
HenryZ6 <99511494+HenryZ6@users.noreply.github.com>
2025-08-14 22:16:39 -0400

    Coded the user log-in interface

--- Main.py
+++ Main.py
@@ -1,17 +1,60 @@
 def main():
-    print("Welcome to the Summer Home Recommender App!")
-    print("If you are a user, type [u]")
-    print("If you are an administrator, type [a]")
-    choice = input("Please select between [u] and [a]: ")
+    app_status = True
+    is_user = False
+    is_admin = False
+    search_status = False
+    logged_in = False
 
-    if choice == "u":
-        print("Welcome user!")
-    
-    elif choice == "a":
-        print("Welcome administrator!")
+    user_username = ""
+    user_password = ""
 
-    else:
-        print("Invalid choice. Exiting.")
-        return
+    while app_status:
+        print("Welcome to the Summer Home Recommender App!")
+        print("If you are a user, type [u]")
+        print("If you are an administrator, type [a]")
+        print("If you want to exit, type [e]")
+        choice_1 = input("[u]/[a]/[e]: ")
+        
+        if choice_1 == "u":
+            is_user = True
+            
+            while is_user:
+                print("Welcome our user! Please choose between the following options:")
+                choice_2 = input("[1]Sign in; [2]Sign up; [3]Exit ")
+
+                if choice_2 == "1":
+                    user_username = input("Please enter your username: ")
+                    user_password = input("Please enter your password: ")
+                    logged_in = True
+
+                elif choice_2 == "2":
+                    print("Let's register a new account for you!")
+                    user_username = input("Please enter your username: ")
+                    user_password = input("Please enter your password: ")
+                    
+                elif choice_2 == "3":
+                    print("Thanks for using!")
+                    is_user = False
+                    app_status = False
+                    break
+                    
+                else:
+                    print("Invalid choice!")
+                    
+                        
+        elif choice_1 == "a":
+            is_admin = True
+            print("Welcome administrator!")
+            
+        
+        elif choice_1 == "e":
+            app_status = False
+            print("Thanks for using!")
+            
+
+        else:
+            print("Invalid choice. Exiting.")
+            return
+
 
 main()
