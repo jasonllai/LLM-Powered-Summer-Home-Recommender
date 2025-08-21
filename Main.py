906a5e5c8d5f9e52e03c0d128cd143809b7c13e4
HenryZ6 <99511494+HenryZ6@users.noreply.github.com>
2025-08-21 00:43:31 -0400

    Completed View User Profile, Edit User Profile, Recommend, User Sign Up functionalities in main; Changed two file names for import statement

--- Main.py
+++ Main.py
@@ -1,11 +1,23 @@
+from numpy import False_
+import pandas as pd
+from Functions import User, Admin, Property
+from Functions import create_user_profile, edit_user_profile, view_user_profile
+from Functions import users_obj_list, property_obj_list
+from Recommender_Logic import ListingRecommender
+from Filter_And_Sort import filter_properties
+from utils import check_login_validity, validate_booking_input
+from LLM_functions import generate_suggestions
+
+
+
+
 def main():
     app_status = True
     is_user = False
     is_admin = False
     search_status = False
-    direct_search_status = False
-    ai_consultant_status = False
     filter_status = False
+    ai_consultant_status = False
     booking_status = False
 
     user_logged_in = False
@@ -14,12 +26,18 @@ def main():
     user_username = ""
     user_password = ""
 
+    curr_user = None
+
+    environment_pool = ["beach", "lake", "forest", "mountains", "nightlife", "remote", "glamping", "city", "modern", "historic"]
+
+
     while app_status:
         print("Welcome to the Summer Home Recommender App!")
         print("If you are a user, type [u]")
         print("If you are an administrator, type [a]")
         print("If you want to exit, type [e]")
         choice_1 = input("[u]/[a]/[e]: ")
+
         
         if choice_1 == "u":
             is_user = True
@@ -31,9 +49,10 @@ def main():
                 if choice_2 == "1":
                     user_username = input("Please enter your username: ")
                     user_password = input("Please enter your password: ")
-                    # Need to check if the username and password pair exists
-                    # Call check_login_validity(user_username, user_password, user_list)
-                    user_logged_in = True
+                    curr_user = check_login_validity(user_username, user_password, users_obj_list)
+                    
+                    if curr_user:
+                        user_logged_in = True 
 
                     while user_logged_in:
                         print("How can we help you today?")
@@ -45,67 +64,147 @@ def main():
                         choice_3 = input("Please enter the corresponding letter: ")
 
                         if choice_3 == "v":
-                            print("Here is your profile: ")
-                            continue
+                            view_user_profile(curr_user.user_id)
+                            
 
                         elif choice_3 == "u":
-                            print("Please follow the instruction to update your profile")
-                            continue
+                             while True:
+                                print(f"\n=== EDITING USIER PROFILE ===")
+                                print("Current profile:")
+                                view_user_profile(curr_user.user_id)
+
+                                new_name = curr_user.name
+                                new_password = curr_user.password
+                                new_group_size = curr_user.group_size
+                                new_preferred_environment = curr_user.preferred_environment
+                                new_budget_range = curr_user.budget_range
+
+                                print("\nWhat would you like to edit?")
+                                print("[1] Name")
+                                print("[2] Password")
+                                print("[3] Create Booking")
+                                print("[4] Delete Booking")
+                                print("[5] Group Size")
+                                print("[6] Preferred Environment")
+                                print("[7] Budget Range")
+                                print("[8] Exit")
+
+                                try:
+                                    edit_choice = int(input("Enter your choice (1-8): ").strip())
+                                    if edit_choice < 1 or edit_choice > 8:
+                                        print("Please enter a number between 1 and 8")
+                                        continue
+
+                                except ValueError:
+                                    print("Please enter a number")
+                                    continue
+
+                                if edit_choice == 8:
+                                    print("Edit session completed")
+                                    return curr_user
+
+                                elif edit_choice == 1:
+                                    new_name = input("New name: ").strip()
+                                    edit_user_profile(curr_user.user_id, edit_choice, new_name)
+                                    
+                                elif edit_choice == 2:
+                                    new_password = input("New password: ").strip()
+                                    edit_user_profile(curr_user.user_id, edit_choice, new_password)
+                                    
+                                elif edit_choice == 3:
+                                    edit_user_profile(curr_user.user_id, edit_choice)
+
+                                elif edit_choice == 4:
+                                    edit_user_profile(curr_user.user_id, edit_choice)
+
+                                elif edit_choice == 5:
+                                    new_group_size = input("New group size: ")
+                                    edit_user_profile(curr_user.user_id, edit_choice, new_group_size)
+
+                                elif edit_choice == 6:
+                                    environment_pool = ["beach", "lake", "forest", "mountains", "nightlife", "remote", "glamping", "city", "modern", "historic"]
+                                    print("Please enter your preferred environment from the list below")
+                                    print("If you would like to check multiple preferred environments, please separate each tags by comma")
+                                    print(environment_pool)
+                                    environment_input = input("New preferred environment: ")
+                                    # Split by comma and strip whitespace
+                                    new_preferred_environment = [tag.strip() for tag in environment_input.split(",") if tag.strip()]
+                                    # Optional: validate against available tags
+                                    new_preferred_environment = [tag for tag in new_preferred_environment if tag in environment_pool]
+
+                                    edit_user_profile(curr_user.user_id, edit_choice, new_preferred_environment)
+
+                                elif edit_choice == 7:
+                                    new_min_budget = input("New minimum budget is: ")
+                                    new_max_budget = input("New maximum budget is: ")
+                                    new_budget_range = (new_min_budget, new_max_budget)
+                                    edit_user_profile(curr_user.user_id, edit_choice, new_budget_range)
+                                                    
 
                         elif choice_3 == "s":
                             search_status = True
 
                             while search_status:
+                                # Call recommender logic function here
+                                recommender = ListingRecommender(property_obj_list)
+                                property_listing_calculated_scores = recommender.calculate_total_score(curr_user)
+                                properties_df = pd.DataFrame(property_listing_calculated_scores)
+                                properties_df["tags"] = properties_df["tags"].apply(lambda x: [tag.strip() for tag in x.split(",")])
+                                properties_df = properties_df.drop(columns=["total_score"])
+                                properties_df["property_index"] = properties_df.index
+                                print(properties_df[["location", "type", "price_per_night", "guest_capacity", "tags"]])
+
                                 print("You can choose to: ")
-                                print("[1] Direct search for properties")
-                                print("[2] Ask our AI consultant")
+                                print("[1] Filter properties")
+                                print("[2] Ask our AI assistant")
                                 print("[3] Go back to home page")
                                 choice_4 = input("Please select: ")
 
                                 if choice_4 == "1":
-                                    direct_search_status = True
+                                    filter_status = True
 
-                                    while direct_search_status:
-                                        # Call recommender function
+                                    while filter_status:
+                                        # Call filter function
+                                        properties_df.to_json("data/properties.json", orient="records", indent=4)
+
+
                                         print("Please enter your preferences: ")
-                                        print("...")  # Display the properties from recommender function
-                                        # Store the returned data frame from recommender function: df = ...
+                                        print("...")  # Display the properties from filter function
+                                        # Store the returned data frame from filter function: df = ...
                                         # Add index to the data frame: df["indexes"] = df.index + 1
-                                        print("[f] Filter recommended properties")
-                                        print("[2] Make a booking by entering the corresponding index: ")
-                                        print("[b] Go back to the previous page")
+                                        print("Have you find an ideal place to stay?")
+                                        print("[b] Make a booking")
+                                        print("[r] Return to the previous page")
                                         choice_5 = input()
 
-                                        if choice_5 == "f":
-                                            # Call filter function; pass df into the filter function
-                                            print("Filtering")
-                                            # df = ...
-                                            # df = df.reset_index(drop=True)
-                                            # df["indexes"] = df.index + 1
+                                        if choice_5 == "b":
+                                            book_index = input("Please enter the index of the property you want to book: ")
+                                            is_valid, row_index = validate_booking_input(book_index, properties_df, id_column="indexes")
 
-                                        elif choice_5 == "2":  # Need to be modified later
-                                            # Have to check if the input is an integer between 1 and last index of the df
-                                            print("Make a booking")
-                                            # Call booking function; should take prop_id and dates as its arguments
-                                            print("Booking successful!")
-                                            direct_search_status = False
-                                            search_status = False
+                                            if is_valid:
+                                                book_prop_id = properties_df.loc[row_index, "property_id"]
+                                                # book_property(book_prop_id); Need to implement a booking function
+                                                print("Make a booking")
+                                                print("Booking successful!")
+                                                direct_search_status = False
+                                                search_status = False
 
-                                        elif choice_5 == "b":
-                                            direct_search_status = False
+                                            else:
+                                                print("Invalid id!")
+
+
+                                        elif choice_5 == "r":
+                                            filter_status = False
 
                                         else:
                                             print("Invalid choice!")
-
-
 
 
                                 elif choice_4 == "2":
                                     ai_consultant_status = True
                                     
                                     while ai_consultant_status:
-                                        # Call LLM function
-                                        print("Let our AI consultant help!")
+                                        generate_suggestions()
                                         ai_consultant_status = False
 
                                 elif choice_4 == "3":
@@ -115,6 +214,7 @@ def main():
                                     print("Invalid choice!")
                         
                         elif choice_3 == "b":
+                            # Need to print current booking list
                             print("Here are your current booking: ")
 
                         elif choice_3 == "e":
@@ -128,7 +228,25 @@ def main():
 
                 elif choice_2 == "2":
                     # create_user_profile()
-                    print("Let's sign up!")
+                    print("\n=== CREATE NEW PROFILE ===")
+                    new_user_name = input("Enter your name: ")
+                    new_user_password = input("Enter your password: ")
+                    new_user_group_size = input("Enter your group size: ")
+
+                    print("Please enter your preferred environment from the list below")
+                    print("If you would like to check multiple preferred environments, please separate each tags by comma")
+                    print(environment_pool)
+                    new_environment_input = input("New preferred environment: ")
+                    # Split by comma and strip whitespace
+                    new_user_preferred_environment = [tag.strip() for tag in new_environment_input.split(",") if tag.strip()]
+                    # Optional: validate against available tags
+                    new_user_preferred_environment = [tag for tag in new_user_preferred_environment if tag in environment_pool]
+
+                    new_user_min_budget = int(input("Enter your minimum budget: "))
+                    new_user_max_budget = int(input("Enter your maximum budget: "))
+                    new_user_budget_range = (new_user_min_budget, new_user_max_budget)
+
+                    create_user_profile(new_user_name, new_user_password, new_user_group_size, new_user_preferred_environment, new_user_budget_range)
                     
                 elif choice_2 == "3":
                     print("Thanks for using!")
@@ -150,8 +268,7 @@ def main():
                 if choice_6 == "1":
                     admin_username = input("Please enter your username: ")
                     admin_password = input("Please enter your password: ")
-                    # Call check_login_validity(admin_username, admin_password, admin_list)
-                    admin_logged_in = True
+                    admin_logged_in = check_login_validity(admin_username, admin_password, users_obj_list)
 
                     while admin_logged_in:
                         print("Welcome Administrator! Please select from the following options: ")
@@ -215,3 +332,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
